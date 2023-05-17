a0189b3dfd6f5e8c2672ab35845eae51bf6aa15c
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-05-17 15:02:17 +0900

    Update MainActivity.kt

--- app/src/main/java/ddwu/com/mobile/adaptereventtest/MainActivity.kt
+++ app/src/main/java/ddwu/com/mobile/adaptereventtest/MainActivity.kt
@@ -14,19 +14,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val dao = SubjectDao()
-        val dataList = dao.dataList
+        val foods = FoodDao().foods
+				val adapter = FoodAdapter(foods)
 
         val layoutManager = LinearLayoutManager(this)
         layoutManager.orientation = LinearLayoutManager.VERTICAL    // 생략 가능
+
         binding.recyclerView.layoutManager = layoutManager
 
         val adapter = MyAdapter(this, R.layout.list_view, dataList)
         binding.recyclerView.adapter = adapter
 
-        binding.btnAdd.setOnClickListener{
-            dataList.add(binding.etText.text.toString())
-            adapter.notifyDataSetChanged()
-        }
     }
 }
