578b1ef735a54a5c2b290775a0a90d352046d97a
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-05-17 15:09:44 +0900

    Update MyAdapter.kt

--- app/src/main/java/ddwu/com/mobile/adaptereventtest/MyAdapter.kt
+++ app/src/main/java/ddwu/com/mobile/adaptereventtest/MyAdapter.kt
@@ -11,19 +11,19 @@ import androidx.recyclerview.widget.RecyclerView
 //BindingFoodAdapter로 이름 바꾸기
 
 class FoodAdapter (val foods : ArrayList<FoodDto>)
-	:RecyclerView.Adapter<FoodAdapter.FooodViewHolder>() {
+	:RecyclerView.Adapter<BindingFoodAdapter.FooodViewHolder>() {
 
    // RecyclerView 에 표시할 전체 뷰의 개수 == 원본 데이터의 개수, 데이터의 개수 확인이 필요할 때 호출
     override fun getItemCount(): Int = foods.size
 
     // 각 항목의 뷰를 보관하는 Holder, 각 item view의 view holder 생성 시 호출
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-        return FoodViewHolder(itemView)
+        val itemView = ListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return FoodViewHolder(itemBinding)
     }
 
     // 항목의 뷰를 생성한 후 멤버변수로 보관하는 ViewHolder
-    class FoodViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class FoodViewHolder(val itemBinding: ListItemBinding) : RecyclerView.ViewHolder(itemBinding.root) {
         val tvText : TextView = view.findViewById(R.id.tvText)
         init {
             // adapter의 notifyDatasetChanged 및 adapter의 멤버변수 list 사용하여 이벤트 처리
@@ -37,9 +37,9 @@ class FoodAdapter (val foods : ArrayList<FoodDto>)
 
     // Holder 에 보관중인 View 에 원본 데이터 연결, 각 item view의 항목에 데이터 결합 시 호출
     override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
-        holder.photo.setImageResource(foods[position].photo)
-				holder.food.text = foods[position].food
-				holder.count.text = foods[position].count.toString()
+			holder.itemBinding.ivPhoto.setImageResource(foods[position].photo)
+			holder.itemBinding.tvFood.text = foods[position].food
+			holder.itemBinding.tvCount.text = foods[position].count.toString()
     }
 
 
