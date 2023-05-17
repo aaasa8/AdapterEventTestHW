dfa462dac737a00ee565c50f705d5f98aa47021a
aaasa8 <113534445+aaasa8@users.noreply.github.com>
2023-05-17 15:00:15 +0900

    Update MyAdapter.kt

--- app/src/main/java/ddwu/com/mobile/adaptereventtest/MyAdapter.kt
+++ app/src/main/java/ddwu/com/mobile/adaptereventtest/MyAdapter.kt
@@ -8,23 +8,22 @@ import android.view.ViewGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
+//BindingFoodAdapter로 이름 바꾸기
 
-class MyAdapter(val context: Context, val layout: Int, val list : ArrayList<String>)
-    : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+class FoodAdapter (val foods : ArrayList<FoodDto>)
+	:RecyclerView.Adapter<FoodAdapter.FooodViewHolder>() {
 
-    // RecyclerView 에 표시할 전체 뷰의 개수 == 원본 데이터의 개수
-    override fun getItemCount(): Int {
-        return list.size
-    }
+   // RecyclerView 에 표시할 전체 뷰의 개수 == 원본 데이터의 개수, 데이터의 개수 확인이 필요할 때 호출
+    override fun getItemCount(): Int = foods.size
 
-    // 각 항목의 뷰를 보관하는 Holder
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val view = LayoutInflater.from(context).inflate(layout, parent, false)
-        return MyViewHolder(this, view)
+    // 각 항목의 뷰를 보관하는 Holder, 각 item view의 view holder 생성 시 호출
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FoodViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        return FoodViewHolder(itemView)
     }
 
     // 항목의 뷰를 생성한 후 멤버변수로 보관하는 ViewHolder
-    class MyViewHolder(adapter: MyAdapter, view: View) : RecyclerView.ViewHolder(view) {
+    class FoodViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val tvText : TextView = view.findViewById(R.id.tvText)
         init {
             // adapter의 notifyDatasetChanged 및 adapter의 멤버변수 list 사용하여 이벤트 처리
@@ -36,11 +35,13 @@ class MyAdapter(val context: Context, val layout: Int, val list : ArrayList<Stri
         }
     }
 
-    // Holder 에 보관중인 View 에 원본 데이터 연결
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.tvText.text = list[position]
+    // Holder 에 보관중인 View 에 원본 데이터 연결, 각 item view의 항목에 데이터 결합 시 호출
+    override fun onBindViewHolder(holder: FoodViewHolder, position: Int) {
+        holder.photo.setImageResource(foods[position].photo)
+				holder.food.text = foods[position].food
+				holder.count.text = foods[position].count.toString()
     }
 
 
-}
 
+}
